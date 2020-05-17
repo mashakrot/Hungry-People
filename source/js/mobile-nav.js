edaab9bcb199565c85dc437ecb4b75cb8d383776
Маша <makrosensei@gmail.com>
2020-05-17 21:49:36 +0300

    Почти все(наконец-то)

--- source/js/mobile-nav.js
+++ source/js/mobile-nav.js
@@ -3,11 +3,11 @@ var navButton = document.querySelector('.navigation-button');
 
 navMain.classList.remove('navigation--nojs');
 navButton.addEventListener('click', function() {
-  if (navMain.classList.contains('navigation--closed')) {
-    navMain.classList.remove('navigation--closed');
-    navMain.classList.add('navigation--opened');
-  } else {
-    navMain.classList.add('navigation--closed');
+  if (navMain.classList.contains('navigation--opened')) {
     navMain.classList.remove('navigation--opened');
+    navMain.classList.add('navigation--closed');
+  } else {
+    navMain.classList.add('navigation--opened');
+    navMain.classList.remove('navigation--closed');
   }
 });
